605b7d03d56f14b74371e7dd253301ec35033d07
Guan Gui <guan@southbanksoftware.com>
2018-01-27 09:53:08 +1100

    Changed AU voltage to 230v

--- EfergyRPI_log.c
+++ EfergyRPI_log.c
@@ -1,10 +1,10 @@
 // EFERGY ENERGY SENSOR RTL-SDR DECODER via rtl_fm
 //
 // Compile:
-//  gcc -O3 -lm -o EfergyDecode EfergyRPI_log.c
+//  gcc -O3 -lm -Wall -o EfergyRPI_log EfergyRPI_log.c
 //
 // Run:
-//  rtl_fm -f 433.25M -s 200000 -r 96000 -A fast -g 19.7 | ./EfergyRPI_log
+//  rtl_fm -f 433.55M -s 200k -r 96k -A fast | ./EfergyRPI_log -d 1
 //
 //------------------------------------------------------------------------------
 // Originally Authored by Nathaniel Elijah
@@ -55,61 +55,61 @@
 // -----------------------------------------------------------------------------
 // Changes by github user magellannh
 //
-// 08-14-2014 - Changed logic to support different Efergy sensor types without 
-// changing settings logic uses message size to determine if checksum or crc 
-// should be used. Also, changed code so instead of doing bit by bit processing 
-// when samples arrive, a frame's worth of samples is saved after a valid 
-// preamble and then processed. One advantage of this approach is that the 
-// center is recalculated for each frame for more reliable decoding, especially 
-// if there's lots of 433Mhz noise, which for me was throwing off the old center 
-// calculation (noise from weather sensors). This is less efficient, but still 
-// uses < 7% of cpu on raspberry pi (plus another 25% for rtl_fm w/ -A fast 
+// 08-14-2014 - Changed logic to support different Efergy sensor types without
+// changing settings logic uses message size to determine if checksum or crc
+// should be used. Also, changed code so instead of doing bit by bit processing
+// when samples arrive, a frame's worth of samples is saved after a valid
+// preamble and then processed. One advantage of this approach is that the
+// center is recalculated for each frame for more reliable decoding, especially
+// if there's lots of 433Mhz noise, which for me was throwing off the old center
+// calculation (noise from weather sensors). This is less efficient, but still
+// uses < 7% of cpu on raspberry pi (plus another 25% for rtl_fm w/ -A fast
 // option)
 // 08-13-2014 - Added code to check the CRC-CCIT (Xmodem)  crc used by Elite 3.0
 // TPM
 // 08/12/2014 - Some debugging and sample analysis code added
 //
-// Bug Fix - Changed frame bytearray to unsigned char and added cast on  byte 
-// used in pow() function to explicitly make it signed char.  Default 
-// signed/unsigned for char is compiler dependent and this resulted in various 
+// Bug Fix - Changed frame bytearray to unsigned char and added cast on  byte
+// used in pow() function to explicitly make it signed char.  Default
+// signed/unsigned for char is compiler dependent and this resulted in various
 // problems.
 //
-// New Feature  - Added frame analysis feature that dumps debug information to 
-// help characterize the FSK sample data received by rtl_fm.  The feature is 
-// invoked using a "-d" option followed by an optional debug level (1..4). The 
+// New Feature  - Added frame analysis feature that dumps debug information to
+// help characterize the FSK sample data received by rtl_fm.  The feature is
+// invoked using a "-d" option followed by an optional debug level (1..4). The
 // output is sent to stdout.
 //
-// Usage Examples: (original examples above still work as before plus these new 
+// Usage Examples: (original examples above still work as before plus these new
 // options)
 //
 //  rtl_fm -f 433.51e6 -s 200000 -r 96000 -A fast  | ./EfergyRPI_log -d 1
-//      This mode shows the least information which is just the best guess at 
-//      the decoded frame and a KW calculation using bytes 4, 5, and 6.  The 
+//      This mode shows the least information which is just the best guess at
+//      the decoded frame and a KW calculation using bytes 4, 5, and 6.  The
 //      checksum is computed and displayed, but not validated.
 //  rtl_fm -f 433.51e6 -s 200000 -r 96000 -A fast  | ./EfergyRPI_log -d 2
-//      This  level shows average plus and minus sample values and centering 
-//      which can help with finding the best frequency. Adjust frequency to get 
-//      wave center close to  0 .  If center is too high, lower frequency, 
+//      This  level shows average plus and minus sample values and centering
+//      which can help with finding the best frequency. Adjust frequency to get
+//      wave center close to  0 .  If center is too high, lower frequency,
 //      otherwise increase it.
 //  rtl_fm -f 433.51e6 -s 200000 -r 96000 -A fast  | ./EfergyRPI_log -d 3
-//      This mode outputs a summary with counts of consecutive positive or 
-//      negative samples.  These consecutive pulse counts are what the main code 
-//      uses to decode 0 and 1 data bits.  See comments below for more details 
+//      This mode outputs a summary with counts of consecutive positive or
+//      negative samples.  These consecutive pulse counts are what the main code
+//      uses to decode 0 and 1 data bits.  See comments below for more details
 //      on expected pulse counts.
 //  rtl_fm -f 433.51e6 -s 200000 -r 96000 -A fast  | ./EfergyRPI_log -d 4
-//      This mode shows everything in modes 1..3 plus a raw dump of the sample 
+//      This mode shows everything in modes 1..3 plus a raw dump of the sample
 //      data received from rtl_fm.
 //
-//  *Notice the "-A fast" option on  rtl_fm.  This cut Raspberry Pi cpu load 
-//  from 50% to 25% and decode still worked fine. Also, with an R820T USB 
+//  *Notice the "-A fast" option on  rtl_fm.  This cut Raspberry Pi cpu load
+//  from 50% to 25% and decode still worked fine. Also, with an R820T USB
 //  dongle, leaving  rtl_fm gain in 'auto' mode  produced the best results.
 // -----------------------------------------------------------------------------
 // Changes by github user guiguan (root@guiguan.net)
 //
 // 18/09/2015 - Added TARGET_UID setting to only capture frames with the
-// targeted unique identifier and drop the rest. This is necessary when your 
+// targeted unique identifier and drop the rest. This is necessary when your
 // neighbours are using the same kind of sensors.
-// Changed the unit from kW to W in debugging output. Also, the threshold from 
+// Changed the unit from kW to W in debugging output. Also, the threshold from
 // 10 to 10000, which means 10kW.
 // Included unistd.h for sleep in linux environment
 // Slightly formatted the source code.
@@ -122,29 +122,24 @@
 #include <string.h>
 #include <unistd.h>
 
-#define VOLTAGE         240 /* For non-TPM type sensors, set to line voltage, 
+#define VOLTAGE         230 /* For non-TPM type sensors, set to line voltage,
 such as 240; For Efergy Elite 3.0 TPM,  set to 1 */
-#define TARGET_UID     -1 /* The 3rd byte of the frame received is used to
-uniquely identify the transmitter. If set, only frames match this UID will be
+#define TARGET_UID 0x0b18bc /* The first 3 bytes of the frame received is used
+to uniquely identify the transmitter. If set, only frames match this UID will be
 captured; otherwise, set to -1 to disable this feature */
 
-#define FRAMEBYTECOUNT          9  /* Attempt to decode up to this many bytes.   */
+#define FRAMEBYTECOUNT          8  /* Attempt to decode up to this many bytes. */
 #define MINLOWBITS              3  /* Min number of positive samples for a logic 0 */
 #define MINHIGHBITS             9  /* Min number of positive samples for a logic 1 */
-#define MIN_POSITIVE_PREAMBLE_SAMPLES       40 /* Number of positive samples in  an efergy  preamble */
+#define MIN_POSITIVE_PREAMBLE_SAMPLES       40 /* Number of positive samples in  an efergy preamble */
 #define MIN_NEGATIVE_PREAMBLE_SAMPLES       40 /* Number of negative samples for a valid preamble  */
 #define EXPECTED_BYTECOUNT_IF_CHECKSUM_USED 8
 #define EXPECTED_BYTECOUNT_IF_CRC_USED      9
 
-int analysis_wavecenter;
-
-#define LOGTYPE         1   // Allows changing line-endings - 0 is for Unix /n, 1 for Windows /r/n
+#define LOGTYPE         0   // Allows changing line-endings - 0 is for Unix /n, 1 for Windows /r/n
 #define SAMPLES_TO_FLUSH    10  // Number of samples taken before writing to file.
 // Setting this too low will cause excessive wear to flash due to updates to
 // filesystem! You have been warned! Set to 10 samples for 6 seconds = every min.
-int loggingok;   // Global var indicating logging on or off
-int samplecount; // Global var counter for samples taken since last flush
-FILE *fp;    // Global var file handle
 
 // Instead of processing frames bit by bit as samples arrive from rtl_fm, all samples are stored once a preamble is detected until
 // enough samples have been saved to cover the expected maximum frame size.  This maximum number of samples needed for a frame
@@ -154,24 +149,28 @@ FILE *fp;    // Global var file handle
 // zero bits are usually received as 10-13 negative samples followed by 4-7 positive samples, while 1 bits
 // come in as 4-7 negative samples followed by 10-13 positive samples ( these #s may have wide tolerences)
 // If the signal has excessive noise, it's theoretically possible to fill up this storage and still have more frame data coming in.
-// The code handles this overflow by trunkating the frame, but when this happens, it usually means the data is  junk anyway.
+// The code handles this overflow by trunkating the frame, but when this happens, it usually means the data is junk anyway.
 //
 // To skip over noise frames, the code checks for a sequence with both a positive and negative preamble back to back (can be pos-neg or neg-pos)
-//  From empirical testing, the preamble is usually about 180 negative samples followed by 45-50 positive samples.
+// From empirical testing, the preamble is usually about 180 negative samples followed by 45-50 positive samples.
 //
-// At some frequencies the sign of the sample data becomes perfectly inverted.  When this happens,  frame data can still be decoded by keying off
+// At some frequencies the sign of the sample data becomes perfectly inverted.  When this happens, frame data can still be decoded by keying off
 // negative pulse sequences instead of positive pulse sequences.  This inversion condition can be detected by looking at the sign of the first samples
 // received after the preamble.  If the first set of samples is negative, the data can get decoded from the positive sample pulses.  If the first set of
-//samples after the preamble is  greater than 0 then the data probably can be found by decoding negative samples using the same rules that are usually
+// samples after the preamble is  greater than 0 then the data probably can be found by decoding negative samples using the same rules that are usually
 // used when decoding with the positive samples.  The analysis code automatically checks for this 'inverted' signal condition and decodes from either
 // the positive or negative pulse stream depending on that check.
 
-#define APPROX_SAMPLES_PER_BIT  19
+#define APPROX_SAMPLES_PER_BIT  20
 #define FRAMEBITCOUNT           (FRAMEBYTECOUNT*8)  /* bits for entire frame (not including preamble) */
 #define SAMPLE_STORE_SIZE       (FRAMEBITCOUNT*APPROX_SAMPLES_PER_BIT)
 
 int sample_storage[SAMPLE_STORE_SIZE];
-int sample_store_index;
+int analysis_wavecenter = 0;
+int sample_store_index = 0;
+int loggingok = 0;   // Global var indicating logging on or off
+int samplecount = 0; // Global var counter for samples taken since last flush
+FILE *fp;    // Global var file handle
 
 int decode_bytes_from_pulse_counts(int pulse_store[], int pulse_store_index, unsigned char bytes[]) {
     int i;
@@ -267,8 +266,6 @@ int generate_pulse_count_array(int display_pulse_details, int pulse_count_storag
     int pulse_count = 0;
     int space_count = 0;
     int pulse_store_index = 0;
-    int space_store_index = 0;
-    int display_pulse_info = 1;
     int i;
     for (i = 0; i < sample_store_index; i++) {
         int samplec = sample_storage[i] - analysis_wavecenter;
@@ -305,8 +302,12 @@ void display_frame_data(int debug_level, char *msg, unsigned char bytes[], int b
 
 #if TARGET_UID > 0
     // only receive frames that has the targeted UID
-    if (bytecount < 3 || bytes[2] != TARGET_UID) {
+    if (bytecount < 3) {
         return;
+    } else {
+        int curr_UID = bytes[0]*65536+bytes[1]*256+bytes[2];
+        if (curr_UID != TARGET_UID)
+            return;
     }
 #endif
 
@@ -358,7 +359,7 @@ void display_frame_data(int debug_level, char *msg, unsigned char bytes[], int b
                 printf("*For Efergy True Power Moniter (TPM), set VOLTAGE=1 before compiling\n");
         }
     } else if (data_ok_str != (char *) 0) {
-        printf("%s,%f\n", buffer, result);
+        printf("total_energy_consumption value=%f %u000000000\n", result, (unsigned)time(NULL));
         if (loggingok) {
             if (LOGTYPE) {
                 fprintf(fp, "%s,%f\r\n", buffer, result);
@@ -372,8 +373,7 @@ void display_frame_data(int debug_level, char *msg, unsigned char bytes[], int b
             }
         }
         fflush(stdout);
-    } else
-        printf("Checksum/CEC Error.  Enable debug output with -d option\n");
+    }
 }
 
 void analyze_efergy_message(int debug_level) {
@@ -425,7 +425,7 @@ void analyze_efergy_message(int debug_level) {
     if (debug_level > 1) printf("\n");
 }
 
-void  main (int argc, char**argv)
+int main(int argc, char**argv)
 {
     int debug_level = 0;
 
@@ -459,11 +459,8 @@ void  main (int argc, char**argv)
 
     if (debug_level > 0)
         printf("\nEfergy Power Monitor Decoder - (debug level %d)\n\n", debug_level);
-    else
-        printf("\nEfergy Energy Monitor Decoder\n\n");
 
     int prvsamp;
-    analysis_wavecenter = 0;
 
     while ( !feof(stdin) ) {
 
@@ -497,7 +494,7 @@ void  main (int argc, char**argv)
         while ( !feof(stdin) ) {
             int cursamp  = (int16_t) (fgetc(stdin) | fgetc(stdin) << 8);
             sample_storage[sample_store_index] = cursamp;
-            if (sample_store_index < (SAMPLE_STORE_SIZE - 1))
+            if (sample_store_index < SAMPLE_STORE_SIZE)
                 sample_store_index++;
             else {
                 analyze_efergy_message(debug_level);
